b6a5b0688311643a34b1d3b2308f50b54c12a9bb
hectorahow <hectorahow@gmail.com>
2024-11-13 14:39:06 -0400

    Updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 import seaborn as sns
 import io
@@ -25,7 +25,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
-mse = np.floor(mean_squared_error(y_test, y_pred))
+mae = np.floor(mean_absolute_error(y_test, y_pred))
 
 @app.route('/')
 def index():
@@ -33,19 +33,18 @@ def index():
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    spend_value = float(request.json['spend_value'])  # Updated to read JSON input
+    spend_value = float(request.json['spend_value'])
     predicted_purchases = int(np.ceil(model.predict([[spend_value]])[0]))
-    cpa1 = (spend_value / predicted_purchases - mse)
-    cpa2 = (spend_value / predicted_purchases + mse)
+    cpa1 = (spend_value / (predicted_purchases + mae))
+    cpa2 = (spend_value / (predicted_purchases - mae))
     
     # Visualization
-    plt.figure(figsize=(10, 6))
-    sns.scatterplot(data=data, x='spend', y='purchases', color='blue', label='Actual Data')
-    sns.lineplot(x=x_test['spend'], y=y_pred, color='red', label='Regression Line')
-    plt.scatter([spend_value], [predicted_purchases], color='green', marker='X', s=100, label='Predicted Purchases')
-    plt.title('Spend vs Purchases with Linear Regression Line and Predicted Points')
-    plt.xlabel('Spend')
-    plt.ylabel('Purchases')
+    plt.figure(figsize=(6,3.8))
+    sns.scatterplot(data=data, x='spend', y='purchases', label='Actual Data', color='blue')
+    plt.scatter(spend_value, predicted_purchases, color='red', s=100, label=f'Predicted for Spend=${spend_value}')
+    plt.title("Spend vs Purchases with Prediction")
+    plt.xlabel("Spend ($)")
+    plt.ylabel("Purchases")
     plt.legend()
 
     # Convert plot to PNG image
@@ -54,7 +53,7 @@ def calculate():
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
     
-    result_text = f'{predicted_purchases} ± {mse:.0f} purchases and a CPA between ${cpa1:.1f} - ${cpa2:.1f} are forecast for a spend of ${spend_value}'
+    result_text = f'{predicted_purchases} ± {mae:.0f} purchases and a CPA between ${cpa1:.1f} - ${cpa2:.1f} are forecast for a spend of ${spend_value}'
 
     return jsonify(result_text=result_text, plot_url=plot_url)
 
